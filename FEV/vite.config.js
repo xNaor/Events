5569aa16359caf96eac67c39aac5d3a97e623b05
xNaor <Naorx49@gmail.com>
2025-05-12 22:29:07 +0300

    Update vite.config.js

--- FEV/vite.config.js
+++ FEV/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.', // חשוב!!
   plugins: [react()],
-    root: '.', // או FEV אם הקוד לא בתיקיית שורש
   build: {
     outDir: 'dist'
   }
-})
+});
